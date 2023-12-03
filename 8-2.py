fad505f7c8433b959ceffc0735456fa79d96b5f4
Hadiyazdanpanah <147479237+Hadiyazdanpanah@users.noreply.github.com>
2023-12-03 23:24:53 +0330

    Add files via upload

---
+++ 8-2.py
@@ -0,0 +1,15 @@
+a=['h','A','D','I','@','12','1','$','y','a','z','d','/','*','35']
+b=[]
+c=[]
+d=[]
+for I in a:
+    if I.isupper():
+        b.append(i)
+     else I.islower():
+         c.append(i)
+     else:
+         d.append(i)
+    print(b)
+    print(c)
+    print(d)
+     
