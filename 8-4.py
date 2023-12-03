aa410e99cd80ef57c2dfe34f054be1913e22149e
Hadiyazdanpanah <147479237+Hadiyazdanpanah@users.noreply.github.com>
2023-12-03 23:25:28 +0330

    Add files via upload

---
+++ 8-4.py
@@ -0,0 +1,6 @@
+a=[12,56,33,1]
+b=[50,40,30,20]
+c=[]
+for I in range(len(a)):
+    c.append(a[i]-b[i])
+print(c)
---
+++ 8-7.py
@@ -0,0 +1,4 @@
+a=['m','na','i','ma']
+b=['y','me','s','in']
+s=zip(a,b)
+print(list(s))
